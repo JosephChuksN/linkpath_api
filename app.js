5c22bbdab8b6238ff5a18380ac0ae1c9591d0356
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-10-13 06:39:45 +0800

    fixing

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const linkRoute = require('./Routes/links')
 //middlewares
 app.use(errorHandler)
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 
 const port = process.env.PORT || 5000
 dotenv.config()
