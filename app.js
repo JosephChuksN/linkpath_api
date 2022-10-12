49dd2778c5d8df4d095ce27c83b0f54d5acba24b
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-10-13 00:12:21 +0800

    fixing

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const cors = require('cors')
 
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
+    allowedHeaders: ['Content-Type'],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
