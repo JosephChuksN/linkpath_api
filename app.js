841074e90f757e184b02369a107276461894ef2f
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-10-13 00:21:16 +0800

    testing

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const errorHandler = require('./middleware/errorHandler')
 const cors = require('cors')
 
 
-const corsOptions ={
-    origin:'*', 
-    allowedHeaders: ['Content-Type'],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'*', 
+//     allowedHeaders: ['Content-Type'],
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 
 const port = process.env.PORT || 5000
 dotenv.config()
@@ -27,7 +27,13 @@ app.use(errorHandler)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.use(cors(corsOptions))
+app.use(cors())
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/links',  authenticateUser, linkRoute)
