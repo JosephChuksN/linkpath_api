9033ee6ec49b049d5d99ae79a20c36af1b8d2c72
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-10-13 06:31:15 +0800

    testing

--- app.js
+++ app.js
@@ -6,34 +6,20 @@ const authenticateUser = require('./middleware/authentication')
 const errorHandler = require('./middleware/errorHandler')
 const cors = require('cors')
 
-
-// const corsOptions ={
-//     origin:'*', 
-//     allowedHeaders: ['Content-Type'],
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-
-const port = process.env.PORT || 5000
-dotenv.config()
-const source = process.env.MONGO_URI
 //Routes
 const authRoute = require('./Routes/auth')
 const linkRoute = require('./Routes/links')
 
-//errors handlers
+//middlewares
 app.use(errorHandler)
-
-app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
 app.use(cors())
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+const port = process.env.PORT || 5000
+dotenv.config()
+const source = process.env.MONGO_URI
+
+
 
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/links',  authenticateUser, linkRoute)
@@ -43,7 +29,7 @@ app.get('/', (req, res)=>{
 const start = async () =>{
     try {
         
-      await  connectDb(source, console.log("server loaded"))
+      connectDb(source, console.log("server loaded"))
       app.listen(port, console.log(`server is running at ${port}...`))
     } catch (error) {
         console.log(error)
