651e222250ab0580621659651e7f7fabc774366d
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-10-14 22:07:50 +0800

    fixing

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const authRoute = require('./Routes/auth')
 const linkRoute = require('./Routes/links')
 
 //middlewares
-app.use(errorHandler)
-app.use(express.json())
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
+app.use(errorHandler)
+app.use(express.json())
 
 const port = process.env.PORT || 5000
 dotenv.config()
